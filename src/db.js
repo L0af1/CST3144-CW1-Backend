5e5dc2ee6d4a8c3aa80e9c240f8557eb0e57eda1
Nick <151399981+L0af1@users.noreply.github.com>
2025-11-15 10:42:39 +0000

    updated db.js

--- src/db.js
+++ src/db.js
@@ -1,1 +1,20 @@
-test
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGO_URI; 
+const client = new MongoClient(uri);
+
+let db;
+
+export async function connectDB() {
+  try {
+    await client.connect();
+    db = client.db("tutorconnect");  
+    console.log("Connected to MongoDB, YIPPEEE!!!");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
+}
+
+export function getDB() {
+  return db;
+}
